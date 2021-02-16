b450223a705c043c99249452832dd307cc62c407
fabienb33 <76611607+fabienb33@users.noreply.github.com>
2021-02-16 18:30:17 +0100

    page publish

--- src/containers/Publish.js
+++ src/containers/Publish.js
@@ -1,33 +1,37 @@
 import axios from "axios";
 import { useState } from "react";
+import Cookies from "js-cookie";
+import { Redirect, useHistory } from "react-router-dom";
 
 const Publish = () => {
+  const token = Cookies.get("userToken");
+  const history = useHistory();
+  console.log(token);
+
   const [title, setTitle] = useState("");
-  const [file, setFile] = useState();
-  const [description, setDescription] = useState();
-  const [price, setPrice] = useState();
-  const [condition, setCondition] = useState();
-  const [brand, setBrand] = useState();
-  const [size, setSize] = useState();
-  const [color, setColor] = useState();
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(true);
+  const [file, setFile] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [condition, setCondition] = useState("");
+  const [brand, setBrand] = useState("");
+  const [size, setSize] = useState("");
+  const [color, setColor] = useState("");
+  const [data, setData] = useState("");
+  const [preview, setPreview] = useState("");
 
   const handleSubmit = async (event) => {
     try {
       event.preventDefault();
 
       const formData = new FormData();
-      formData.append("title", title);
-      formData.append("description", description);
-      formData.append("price", price);
+      formData.append("product_name", title);
+      formData.append("product_description", description);
+      formData.append("product_price", price);
       formData.append("condition", condition);
       formData.append("brand", brand);
       formData.append("size", size);
       formData.append("color", color);
-      formData.append("picture", file);
-
-      const token = "13940390429024";
+      formData.append("secure_url", file);
 
       const response = await axios.post(
         "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
@@ -38,89 +42,100 @@ const Publish = () => {
           },
         }
       );
+      if (response.data._id) {
+        history.push(`/offer/${response.data._id}`);
+      }
+
       console.log(response.data);
       setData(response.data);
-      setIsLoading(false);
     } catch (error) {
       console.log(error.message);
     }
   };
 
-  return (
-    <div>
-      <h1>Vends ton article</h1>
-      <input
-        multiple={true}
-        type="file"
-        onChange={(event) => {
-          // console.log(event);
-          setFile(event.target.files[0]);
-        }}
-      />
-      <form onSubmit={handleSubmit}>
-        <p className="titlesPublish">Titre</p>
-        <input
-          type="text"
-          onChange={(event) => {
-            setTitle(event.target.value);
-          }}
-        />
-        <p className="titlesPublish">Décris ton article</p>
-        <input
-          type="text"
-          onChange={(event) => {
-            setDescription(event.target.value);
-          }}
-        />
-        <p className="titlesPublish">Prix</p>
-        <input
-          type="text"
-          onChange={(event) => {
-            setPrice(event.target.value);
-          }}
-        />
-        <p className="titlesPublish">Etat</p>
-        <input
-          type="text"
-          onChange={(event) => {
-            setCondition(event.target.value);
-          }}
-        />
-        <p className="titlesPublish">Marque</p>
-        <input
-          type="text"
-          onChange={(event) => {
-            setBrand(event.target.value);
-          }}
-        />
-        <p className="titlesPublish">Taille</p>
-        <input
-          type="text"
-          onChange={(event) => {
-            setSize(event.target.value);
-          }}
-        />
-        <p className="titlesPublish">Couleur</p>
-        <input
-          type="text"
-          onChange={(event) => {
-            setColor(event.target.value);
-          }}
-        />
+  return token ? (
+    <div className="bgFormulaire">
+      <h1 className="titleForm">Vends ton article</h1>
+      <div className="formulaire">
+        <div className="bgFormPicture">
+          <label htmlFor="file">
+            <span>toto</span>
+          </label>
+          <input
+            multiple={true}
+            id="file"
+            type="file"
+            className="dlPicture"
+            onChange={(event) => {
+              // console.log(event);
+              setFile(event.target.files[0]);
+              setPreview(URL.createObjectURL(event.target.files[0]));
+            }}
+          />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="inputVendre">
+            <p className="titlesPublish">Titre</p>
+            <input
+              type="text"
+              className="inputVendre"
+              onChange={(event) => {
+                setTitle(event.target.value);
+              }}
+            />
+          </div>
+          {preview ? <img src={preview} className="preview" /> : <span></span>}
+          <p className="titlesPublish">Décris ton article</p>
+          <input
+            type="text"
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+          />
+          <p className="titlesPublish">Prix</p>
+          <input
+            type="text"
+            onChange={(event) => {
+              setPrice(event.target.value);
+            }}
+          />
+          <p className="titlesPublish">Etat</p>
+          <input
+            type="text"
+            onChange={(event) => {
+              setCondition(event.target.value);
+            }}
+          />
+          <p className="titlesPublish">Marque</p>
+          <input
+            type="text"
+            onChange={(event) => {
+              setBrand(event.target.value);
+            }}
+          />
+          <p className="titlesPublish">Taille</p>
+          <input
+            type="text"
+            onChange={(event) => {
+              setSize(event.target.value);
+            }}
+          />
+          <p className="titlesPublish">Couleur</p>
+          <input
+            type="text"
+            onChange={(event) => {
+              setColor(event.target.value);
+            }}
+          />
 
-        <br />
+          <br />
 
-        <button type="submit">Envoyer</button>
-      </form>
-      {isLoading ? (
-        <p>En cours de chargement</p>
-      ) : (
-        <>
-          <span>{data.title}</span>
-          <img src={data.url} alt={data.title} />
-        </>
-      )}
+          <button type="submit">Envoyer</button>
+        </form>
+      </div>
     </div>
+  ) : (
+    <Redirect to="/login" />
   );
 };
 
